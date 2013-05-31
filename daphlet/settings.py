2fa4bb758c97925618fed53a3166f190ea33f329
Tony O'Connor <toconnor13@gmail.com>
2013-05-31 16:58:06 +0200

    Settings adjusted to run locally -3

--- daphlet/settings.py
+++ daphlet/settings.py
@@ -181,15 +181,15 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-if not bool(os.environ.get('LOCAL_DEV', False)):
+#if bool(os.environ.get('LOCAL_DEV', True)):
 
-	DATABASES = {
-    		'default': {
-        		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        		'NAME': '/home/sheefrex/code/daphlet_db.db',                      # Or path to database file if using sqlite3.
-        		'USER': '',                      # Not used with sqlite3.
-        		'PASSWORD': '',                  # Not used with sqlite3.
-        		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    			}
-		}
+#	DATABASES = {
+ #   		'default': {
+  #      		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        		'NAME': '/home/sheefrex/code/daphlet_db.db',                      # Or path to database file if using sqlite3.
+#        		'USER': '',                      # Not used with sqlite3.
+#        		'PASSWORD': '',                  # Not used with sqlite3.
+#        		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+ #   			}
+#		}
