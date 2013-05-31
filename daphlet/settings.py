3d13519d1e11fc234b28c1d84978d63302d8881f
Tony O'Connor <toconnor13@gmail.com>
2013-05-31 17:11:41 +0200

    Settings adjusted to run locally -6

--- daphlet/settings.py
+++ daphlet/settings.py
@@ -192,4 +192,4 @@ if bool(os.environ.get('LOCAL_DEV', True)):
        		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
        		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
  			}
-#		}
+		}
